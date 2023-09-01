2bb98c40e7ee57ca7cb4c54c6cd0a7d10606883f
Chitranjan Gupta <37315561+chitranjan-gupta@users.noreply.github.com>
2023-09-01 13:12:50 +0000

    Added Wallet, Connection, Login, Sign Up Page

--- client/components/login.js
+++ client/components/login.js
@@ -4,8 +4,8 @@ import { QRCodeSVG } from "qrcode.react";
 export default function Page() {
   const [qrValue, setQrValue] = useState("hello");
   return (
-    <div>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="overflow-hidden flex flex-row justify-center items-center w-full h-full">
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 overflow-hidden">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign in to your account
