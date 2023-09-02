fd740db6e5b455de3fd9d6eb4a169fa29c752658
Chitranjan Kumar Gupta <chitranjankumargupta123@gmail.com>
2023-09-02 13:21:43 +0000

    Added React Native client using Expo

---
+++ client-app/index.js
@@ -0,0 +1,5 @@
+import 'react-native-get-random-values'
+import { registerRootComponent } from 'expo';
+import App from './App.js';
+
+registerRootComponent(App);
---
+++ client-app/screens/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, View, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+
+    },
+});
+
+export default function Home() {
+    const navigation = useNavigation();
+    return (
+        <View style={styles.container}>
+            <Button title='Scan' onPress={() => navigation.navigate("Scanner")} />
+        </View>
+    );
+}
